770b1ce9b1d689c2fce5addaa1f6315856a6ec57
ahza64 <justinmichaelpettit@gmail.com>
2022-04-22 14:26:44 -0700

    fetch data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useEffect } from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function fetchData(url) {
+    return fetch(url)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        console.log("data", data);
+        return data
+      })
+      .catch((err) => {
+        console.error('Error: ', err)
+      })
+  }
+
+  useEffect(() => {
+    fetchData('/datajson.json')
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
