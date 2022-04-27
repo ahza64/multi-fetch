5921bc10ab7c84a91c52e80bcece8adfe5542140
ahza64 <justinmichaelpettit@gmail.com>
2022-04-27 15:56:50 -0700

    separate test data into seperate file

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@
 // import fetchXML from './core/fetchXML'
 // import fetchData from './core/fetchData'
 
+import personsInputData from './testData/appTestData'
 import buildPersonsList from './core/getAllAsyncData'
 import flattenSortList from './core/flattenSortList'
 import buildJSONList from './core/buildJSONList'
@@ -9,82 +10,6 @@ import buildXMLList from './core/buildXMLList'
 import findChildren from './core/findChildren'
 
 describe("unit tests", () => {
-
-  const personsInputData = [
-    {
-      value: {
-        person: [
-          {
-            id: "10",
-            firstName: "joe",
-            lastName: "test"
-          },
-          {
-            id: "5",
-            firstName: "bob",
-            lastName: "test"
-          },
-          {
-            id: "7",
-            firstName: "tom",
-            lastName: "test"
-          }
-        ]
-      }
-    },
-    {
-      value: [
-        {
-          children: [
-            {
-              name: "id",
-              value: "3"
-            },
-            {
-              name: "firstName",
-              value: "susan"
-            },
-            {
-              name: "lastName",
-              value: "test"
-            }
-          ]
-        },
-        {
-          children: [
-            {
-              name: "id",
-              value: "6"
-            },
-            {
-              name: "firstName",
-              value: "kathy"
-            },
-            {
-              name: "lastName",
-              value: "test"
-            }
-          ]
-        },
-        {
-          children: [
-            {
-              name: "id",
-              value: "1"
-            },
-            {
-              name: "firstName",
-              value: "jo"
-            },
-            {
-              name: "lastName",
-              value: "test"
-            }
-          ]
-        }
-      ]
-    }
-  ]
 
   it("flattens and sorts the array", () => {
     const sortedList = flattenSortList(personsInputData[0].value.person)
---
+++ src/testData/appTestData.js
@@ -0,0 +1,77 @@
+const personsInputData = [
+  {
+    value: {
+      person: [
+        {
+          id: "10",
+          firstName: "joe",
+          lastName: "test"
+        },
+        {
+          id: "5",
+          firstName: "bob",
+          lastName: "test"
+        },
+        {
+          id: "7",
+          firstName: "tom",
+          lastName: "test"
+        }
+      ]
+    }
+  },
+  {
+    value: [
+      {
+        children: [
+          {
+            name: "id",
+            value: "3"
+          },
+          {
+            name: "firstName",
+            value: "susan"
+          },
+          {
+            name: "lastName",
+            value: "test"
+          }
+        ]
+      },
+      {
+        children: [
+          {
+            name: "id",
+            value: "6"
+          },
+          {
+            name: "firstName",
+            value: "kathy"
+          },
+          {
+            name: "lastName",
+            value: "test"
+          }
+        ]
+      },
+      {
+        children: [
+          {
+            name: "id",
+            value: "1"
+          },
+          {
+            name: "firstName",
+            value: "jo"
+          },
+          {
+            name: "lastName",
+            value: "test"
+          }
+        ]
+      }
+    ]
+  }
+]
+
+export default personsInputData
